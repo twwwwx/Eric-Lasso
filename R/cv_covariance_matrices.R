bbbb3385c22738d13002e6580aa112885359a7c1
Wenxi Tan <w1229w@126.com>
2023-11-09 22:53:25 +0800

    Multiple plots and RDA

--- R/cv_covariance_matrices.R
+++ R/cv_covariance_matrices.R
@@ -89,10 +89,11 @@ cv_covariance_matrices <- function(K,
 
       # Calculating the nearest PSD cov matrix for the kth fold, to calculate the error on the problem solved without the kth fold
       mat_test <- mat[index, ]
-      cov_modified_test <- 1 / n_one_fold * t(mat_test) %*% mat_test - Sig_B
-      if (mode == "ADMM") {
-        mat_cov_test <- ADMM_proj(cov_modified_test, mu = mu, etol = etol)$mat
+      if (is.vector(mat_test)) {
+        mat_test <- matrix(mat_test, nrow = 1)
       }
+      cov_modified_test <- 1 / n_one_fold * t(mat_test) %*% mat_test - Sig_B
+      mat_cov_test <- ADMM_proj(cov_modified_test, mu = mu, etol = etol)$mat
 
       list_matrices_error <- rlist::list.append(list_matrices_error, mat_cov_test)
 
@@ -104,54 +105,6 @@ cv_covariance_matrices <- function(K,
       # Calculating the surrogate rho for the kth fold, to calculate the error on the problem solved without the kth fold
       y_test <- y[index, ]
       rho_test <- 1 / n_one_fold * t(mat_test) %*% y_test
-      list_rho_error <- rlist::list.append(list_rho_error, rho_test)
-    }
-  } else if (noise == "missing") {
-    # We calculate the global nearest PSD cov matrix and the global surrogate rho, when we take into account the whole data set
-    # mat_for_adjustment <- diag(probs*(1-probs),p,p) + (1-probs) %*% t(1 - probs)
-    # print("Doing the global data")
-    cov_modified <- 1 / n * t(mat) %*% mat / ratio_matrix
-    if (mode == "ADMM") {
-      sigma_global <- ADMM_proj(cov_modified, mu = mu, etol = etol)$mat
-    }
-
-    rho_global <- 1 / n * t(mat) %*% y / diag(ratio_matrix)
-
-    for (i in 1:K) {
-      # We calculate the necessary matrices for the cross validation
-
-      # print(paste("Doing the", i, "fold"))
-      index <- which(folds == i, arr.ind = TRUE)
-
-      # Calculating the nearest PSD cov matrix when we remove the kth fold, to resolve lasso problem during cross validation
-      mat_train <- mat[-index, ]
-      # cov_modified_train <- 1/n_without_fold*t(train_mat)%*%train_mat / mat_for_adjustment
-      cov_modified_train <- 1 / n_without_fold * t(mat_train) %*% mat_train / ratio_matrix
-      if (mode == "ADMM") {
-        mat_cov_train <- ADMM_proj(cov_modified_train, mu = mu, etol = etol)$mat
-      }
-      list_matrices_lasso <- rlist::list.append(list_matrices_lasso, mat_cov_train)
-
-      # Calculating the nearest PSD cov matrix for the kth fold, to calculate the error on the problem solved without the kth fold
-      mat_test <- mat[index, ]
-      # cov_modified_test <- 1/n_one_fold*t(test_mat)%*%test_mat / mat_for_adjustment
-      cov_modified_test <- 1 / n_one_fold * t(mat_test) %*% mat_test / ratio_matrix
-      if (mode == "ADMM") {
-        mat_cov_test <- ADMM_proj(cov_modified_test, mu = mu, etol = etol)$mat
-      }
-
-      list_matrices_error <- rlist::list.append(list_matrices_error, mat_cov_test)
-
-      # Calculating the surrogate rho when we remove the kth fold, to resolve lasso problem during cross validation
-      y_train <- y[-index, ]
-      # rho_train <- 1/n_without_fold * t(train_mat)%*%y_train / (1-probs)
-      rho_train <- 1 / n_without_fold * t(mat_train) %*% y_train / diag(ratio_matrix)
-      list_rho_lasso <- rlist::list.append(list_rho_lasso, rho_train)
-
-      # Calculating the surrogate rho for the kth fold, to calculate the error on the problem solved without the kth fold
-      y_test <- y[index, ]
-      # rho_test <- 1/n_one_fold * t(test_mat)%*%y_test / (1-probs)
-      rho_test <- 1 / n_one_fold * t(mat_test) %*% y_test / diag(ratio_matrix)
       list_rho_error <- rlist::list.append(list_rho_error, rho_test)
     }
   }
--- simulation/eval.R
+++ simulation/eval.R
@@ -6,22 +6,27 @@ library("boot")
 # --------------------------
 
 # simulation settings
-# data_type <- "lognormal"
-data_type <- "dirichlet"
+data_type <- "lognormal"
+# data_type <- "dirichlet"
 # data_type <- "multinom"
 # data_type <- "dirmult"
-N_sim <- 500
-np_list <- list(c(500, 200))
+N_sim <- 100
+# n <- 50
+# p <- 50
+# create a list of different n and p values
+np_list <- list(c(100, 100))
 
 sigma <- 0.5
 rho <- 0.5
-tau <- 0.5
+tau <- 1.5
+
 
 # model settings
 model_list <- list()
-model_list[["eic"]] <- c(TRUE, TRUE)
-model_list[["coda"]] <- c(TRUE, FALSE)
-model_list[["coco"]] <- c(FALSE, TRUE)
+model_list[["Eric"]] <- c(TRUE, TRUE)
+# model_list[["CoDA"]] <- c(TRUE, FALSE)
+# model_list[["CoCo"]] <- c(FALSE, TRUE)
+# model_list[["Vani"]] <- c(FALSE, FALSE)
 # --------------------------
 file_name <- "results/results_table.csv"
 file_name_sum <- "results/results_sum.csv"
@@ -46,11 +51,12 @@ for (np in np_list) {
         constrain <- model[1]
         proj <- model[2]
         subdir <- "results/"
-        subdir_name <- paste0(subdir, model_name, "+", data_type, "_n", n, "_p", p, "_tau", tau, "_rho", rho, "_sigma", sigma, "_Nsim", N_sim)
         if (data_type == "multinom" || data_type == "dirmult") {
-            Sig_B_estimated <- MC_varB(n, p, beta_star, sigma, rho, theta = theta, N_MK = 1000000 %/% p)
+            Sig_B_estimated <- MC_varB(n, p, beta_star, sigma, rho, theta = theta, N_MK = 1000000 %/% p, type = data_type, overdispersion = 5e+3)
+            tau <- sqrt(Sig_B_estimated[6, 6])
             print(paste("estimated tau is", sqrt(Sig_B_estimated[6, 6])))
         }
+        subdir_name <- paste0(subdir, model_name, "+", data_type, "_n", n, "_p", p, "_tau", tau, "_rho", rho, "_sigma", sigma, "_Nsim", N_sim)
 
         # --------------------------
         # run lasso code
@@ -65,7 +71,7 @@ for (np in np_list) {
             # generate data
 
             if (data_type == "multinom" || data_type == "dirmult") {
-                data <- generate_multinom_data(n, p, beta_star, sigma, rho, theta = theta)
+                data <- generate_multinom_data(n, p, beta_star, sigma, rho, theta = theta, type = data_type, overdispersion = 5e+3)
                 data$Sig_B <- Sig_B_estimated
             } else {
                 data <- generate_data(n, p, beta_star, sigma, tau, rho, theta, type = data_type)
@@ -108,13 +114,13 @@ for (np in np_list) {
 
 
         lam_value <- round(bootstrap_mean[1], 2)
-        SE_value <- paste0(round(bootstrap_mean[2], 4), "(", round(bootstrap_mean_std[2], 4), ")")
-        PE_value <- paste0(round(bootstrap_mean[3], 4), "(", round(bootstrap_mean_std[3], 4), ")")
-        l_inf_value <- paste0(round(bootstrap_mean[4], 4), "(", round(bootstrap_mean_std[4], 4), ")")
-        FPR_value <- paste0(round(bootstrap_mean[5], 4), "(", round(bootstrap_mean_std[5], 4), ")")
-        FNR_value <- paste0(round(bootstrap_mean[6], 4), "(", round(bootstrap_mean_std[6], 4), ")")
+        SE_value <- paste0(round(bootstrap_mean[2], 3), "(", round(bootstrap_mean_std[2], 3), ")")
+        PE_value <- paste0(round(bootstrap_mean[3], 3), "(", round(bootstrap_mean_std[3], 3), ")")
+        l_inf_value <- paste0(round(bootstrap_mean[4], 3), "(", round(bootstrap_mean_std[4], 3), ")")
+        FPR_value <- paste0(round(bootstrap_mean[5], 3), "(", round(bootstrap_mean_std[5], 3), ")")
+        FNR_value <- paste0(round(bootstrap_mean[6], 3), "(", round(bootstrap_mean_std[6], 3), ")")
         values <- t(as.matrix(c(model_name, data_type, n, p, N_sim, tau, rho, lam_value, SE_value, PE_value, l_inf_value, FPR_value, FNR_value)))
-        sum_value <- paste0(round(bootstrap_mean[7], 10), "(", round(bootstrap_mean_std[7], 10), ")")
+        sum_value <- paste0(round(bootstrap_mean[7], 3), "(", round(bootstrap_mean_std[7], 3), ")")
         sum_values <- t(as.matrix(c(model_name, data_type, n, p, N_sim, tau, rho, sum_value, p_value)))
 
         write.table(values,
